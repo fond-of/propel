7954a3910150d5f6d0528a88f910e74eb5219cb1
Chris Egger <christopher.egger@fondof.de>
2020-01-16 16:16:21 +0100

    add unit tests

---
+++ tests/unit/DropIndexDDLTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class DropIndexDDLTest extends Codeception\Test\Unit
+{
+    /**
+     * @return void
+     */
+    public function test_ddl_when_index_is_unique()
+    {
+        $platform = new \FondOf\Propel\Generator\Platform\PgsqlPlatform();
+        $index = new \Propel\Generator\Model\Unique();
+        $index->setName('foo-bar');
+        $ddl = $platform->getDropIndexDDL($index);
+
+        $this->assertEquals('DROP INDEX "foo-bar";', $ddl);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_ddl_when_index_is_not_unique()
+    {
+        $platform = new \FondOf\Propel\Generator\Platform\PgsqlPlatform();
+        $mock = $this->getMockBuilder('\Propel\Generator\Model\Index');
+        $index = $mock->setMethods(['getFQName'])->getMock();
+        $index->method('getFQName')->willReturn('foo-bar');
+        $ddl = $platform->getDropIndexDDL($index);
+
+        $this->assertEquals('
+DROP INDEX "foo-bar";
+', $ddl);
+    }
+}
