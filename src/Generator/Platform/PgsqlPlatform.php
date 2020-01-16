29af4c7a2bd83ee7582cef736d2981e76ca6a372
Chris Egger <christopher.egger@fondof.de>
2020-01-16 10:14:49 +0100

    initial commit

---
+++ src/Generator/Platform/PgsqlPlatform.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FondOf\Propel\Generator\Platform;
+
+use Propel\Generator\Model\Index;
+use Propel\Generator\Model\Unique;
+use Propel\Generator\Platform\PgsqlPlatform as PropelPgsqlPlatform;
+
+class PgsqlPlatform extends PropelPgsqlPlatform
+{
+   /**
+    * Overrides the implementation from PropelPsqlPlatform
+    *
+    * @param \Propel\Generator\Model\Index
+    *
+    * @return string
+    */
+    public function getDropIndexDDL(Index $index)
+    {
+        if ($index instanceof Unique) {
+            $pattern = "DROP INDEX %s.%s;";
+
+            return sprintf(
+                $pattern,
+                $this->quoteIdentifier($index->getTable()->getName()),
+                $this->quoteIdentifier($index->getName())
+            );
+        } else {
+            return parent::getDropIndexDDL($index);
+        }
+    }
+}
