5ffe8116848f29b0ebe23f835919c7f82b580ff8
Chris Egger <christopher.egger@fondof.de>
2020-01-17 08:40:21 +0100

    improve maintainability

--- src/Generator/Platform/PgsqlPlatform.php
+++ src/Generator/Platform/PgsqlPlatform.php
@@ -8,6 +8,12 @@ use Propel\Generator\Platform\PgsqlPlatform as PropelPgsqlPlatform;
 
 class PgsqlPlatform extends PropelPgsqlPlatform
 {
+    const DROP_INDEX_DDL_PATTERN = <<<EOT
+
+DROP INDEX %s;
+
+EOT;
+
    /**
     * Overrides the implementation from PropelPsqlPlatform
     *
@@ -15,10 +21,10 @@ class PgsqlPlatform extends PropelPgsqlPlatform
     *
     * @return string
     */
-    public function getDropIndexDDL(Index $index)
+    public function getDropIndexDDL(Index $index): string
     {
         if ($index instanceof Unique) {
-            $pattern = "DROP INDEX %s;";
+            $pattern = static::DROP_INDEX_DDL_PATTERN;
 
             return sprintf(
                 $pattern,
--- tests/unit/DropIndexDDLTest.php
+++ tests/unit/DropIndexDDLTest.php
@@ -6,20 +6,22 @@ class DropIndexDDLTest extends Codeception\Test\Unit
     /**
      * @return void
      */
-    public function test_ddl_when_index_is_unique()
+    public function test_ddl_when_index_is_unique(): void
     {
         $platform = new \FondOf\Propel\Generator\Platform\PgsqlPlatform();
         $index = new \Propel\Generator\Model\Unique();
         $index->setName('foo-bar');
         $ddl = $platform->getDropIndexDDL($index);
 
-        $this->assertEquals('DROP INDEX "foo-bar";', $ddl);
+        $this->assertEquals('
+DROP INDEX "foo-bar";
+', $ddl);
     }
 
     /**
      * @return void
      */
-    public function test_ddl_when_index_is_not_unique()
+    public function test_ddl_when_index_is_not_unique(): void
     {
         $platform = new \FondOf\Propel\Generator\Platform\PgsqlPlatform();
         $mock = $this->getMockBuilder('\Propel\Generator\Model\Index');
